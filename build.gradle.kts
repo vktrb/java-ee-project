e2e5cf49a0fd70224b3ec69aa3435619ad4ac102
fl4methr0wer <vv.boichenko.vv@gmail.com>
2025-10-06 17:29:30 +0200

    Disable spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     java
     war
-    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "pl.lodz.uni"
@@ -16,22 +15,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.build {
-    dependsOn(tasks.spotlessCheck)
-}
-
 tasks.test {
     useJUnitPlatform()
-}
-
-spotless {
-    java {
-        googleJavaFormat("1.17.0") // use Google Java Format with JDK17 support
-        target("src/**/*.java")    // format all Java files
-    }
-    format("misc") {
-        target("*.gradle", "*.md", ".gitignore")
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
 }
